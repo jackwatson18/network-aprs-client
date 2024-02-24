52b11bde86f4b958e8c1ed92202065832e8a54b3
Jack Watson <jackhwatson18@gmail.com>
2024-02-24 15:29:05 -0700

    AX.25 parsing to get a raw APRS field

--- main.go
+++ main.go
@@ -3,26 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
+
+type APRS_Address struct {
+	callsign string
+	Ssid     uint8 // AX.25 spec only gives 8 bytes
+}
+
+type AX25_struct struct {
+	dst  APRS_Address // dst is not really needed for APRS but we will retain it for compatibility
+	src  APRS_Address
+	path []APRS_Address
+	raw  string
+}
 
 func PrintHexBytes(input []byte) {
 	for i := 0; i < len(input); i++ {
-		fmt.Printf("%c ", input[i])
+		fmt.Printf("%c ", input[i]>>1)
 	}
 	fmt.Print("\n")
-	fmt.Println(string(input))
+	// fmt.Println(string(input))
 }
 
-func ParseAX25(raw_packet []byte) {
-	packet := raw_packet[2 : len(raw_packet)-1]
-	// strip the "end frame" chars from packet
-	dst_callsign := packet[0:7]
-	src_callsign := packet[7:15]
-	for i := 0; i < 7; i++ {
-		dst_callsign[i] = dst_callsign[i] >> 1
-		src_callsign[i] = src_callsign[i] >> 1
+func ParseAX25Address(raw []byte) APRS_Address {
+	output := make([]byte, len(raw))
+	for i, char := range raw {
+		output[i] = char >> 1
 	}
-	PrintHexBytes(packet)
+
+	call := strings.TrimSpace(string(output[0:6]))
+	address := APRS_Address{callsign: call, Ssid: uint8(output[6] & 0xf)}
+
+	return address
+}
+
+func ParseAX25(raw_frame []byte) {
+	frame := raw_frame[:len(raw_frame)-1] // strip control headers
+	PrintHexBytes(frame)
+	ax := AX25_struct{}
+	// left shift the src/dst bytes according to AX.25 standard
+	dst := ParseAX25Address(frame[1:8])
+	src := ParseAX25Address(frame[9:16])
+	// fmt.Println(src)
+	// fmt.Println(dst)
+	ax.dst = dst
+	ax.src = src
+	frame = frame[16:]
+	for len(frame) > 7 && frame[0] != 3 {
+		ax.path = append(ax.path, ParseAX25Address(frame[:7]))
+		frame = frame[7:]
+	}
+	ax.raw = string(frame[2:])
+	fmt.Println(ax)
 
 }
 
