aacc951b85503e93aad96c5aa783e52c45c00a77
Jack Watson <jackhwatson18@gmail.com>
2024-10-21 15:10:34 -0600

    test(KISSServerConnector): basic unit tests

--- network_aprs_test.go
+++ network_aprs_test.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"net"
 	"testing"
+	"time"
 )
 
-func Test_test(t *testing.T) {
-	if 1+1 != 2 {
-		t.Error("Something has gone horribly wrong...")
+func Test_KISSServerConnector_failsOnBadTarget(t *testing.T) {
+
+	srv := "badresolver.kk7ewj.net:0000"
+
+	_, _, err := KISSServerConnector(srv)
+
+	if err != nil {
+		// expected behavior
+		return
+	} else {
+		t.Errorf("Expected an error to throw for bad srv addr, but got none")
 	}
+
+}
+
+func Test_KISSServerConnector_goodConnection(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer func() {
+		t.Log("Closing listener")
+		listener.Close()
+	}()
+	srv := listener.Addr().String()
+	t.Log(srv)
+
+	_, error_chan, err := KISSServerConnector(srv)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case err := <-error_chan:
+		t.Fatal(err)
+	case <-time.After(time.Second * 1):
+		return // expected behavior
+	}
+
 }
