af2c6973d118e5dc7a938e454aaf5c0155808a22
Jack Watson <jackhwatson18@gmail.com>
2024-10-22 20:13:58 -0600

    feat(KISSServerEmulator): create emulator for testing

--- network_aprs.go
+++ network_aprs.go
@@ -10,16 +10,51 @@ import (
 	"github.com/fatih/color"
 )
 
+// emulates a KISS server for testing purposes
+func KISSServerEmulator(conn_chan chan net.Conn, input_chan chan []byte) (chan bool, chan error) {
+	done_channel := make(chan bool)
+	err_channel := make(chan error)
+
+	// spin up a goroutine to handle channel inputs
+	go func() {
+		for {
+			select {
+			case <-done_channel:
+				return
+			case conn := <-conn_chan:
+				// spin up a goroutine to send data from the channel to the requestor
+				go func() {
+					for {
+						data, open := <-input_chan
+						if !open {
+							return
+						}
+
+						_, err := conn.Write(data)
+						if err != nil {
+							err_channel <- err
+							return
+						}
+					}
+				}()
+			}
+		}
+	}()
+
+	return done_channel, err_channel
+}
+
 // connects to, and listens to a KISS server. Returns a channel. Meant to run as a goroutine.
 func KISSServerConnector(server string) (chan AX25.AX25_frame, chan error, error) {
-	frame_chan := make(chan AX25.AX25_frame)
-	error_chan := make(chan error)
 
 	conn, err := net.Dial("tcp", server)
 
 	if err != nil {
-		return frame_chan, error_chan, fmt.Errorf("KISSServerConnector: %v", err)
+		return nil, nil, fmt.Errorf("KISSServerConnector: %v", err)
 	}
+
+	frame_chan := make(chan AX25.AX25_frame)
+	error_chan := make(chan error)
 
 	// start the connection listener in a goroutine
 	go func() {
