23ce1ff719e2e2a3d5e28112317e5770f008517b
Jack Watson <jack@jackwatson.dev>
2025-10-12 21:16:36 -0600

    feat: APRS-IS client

--- network_aprs.go
+++ network_aprs.go
@@ -6,6 +6,8 @@ import (
 	"internal/AX25"
 	"log"
 	"net"
+	"strings"
+	"time"
 
 	"github.com/fatih/color"
 )
@@ -51,6 +53,47 @@ func ListenOnlyKISS(server string) (<-chan string, <-chan error, error) {
 	return tnc2_out, err_out, nil
 }
 
+func ListenOnlyAPRSIS() (<-chan string, <-chan error, error) {
+	// Connect to an APRS-IS server (e.g., rotate.aprs2.net:14580 for a filtered feed)
+	conn, err := net.DialTimeout("tcp", "rotate.aprs2.net:14580", 5*time.Second)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error connecting: %w", err)
+	}
+	tnc2_out := make(chan string)
+	err_out := make(chan error)
+	buffer := make([]byte, 1024)
+
+	_, err = conn.Write([]byte("user N0CALL pass -1 filter r/35.28/-120.66/100\n"))
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("error authenticating: %w", err)
+	}
+
+	go func() {
+		defer conn.Close()
+		defer close(tnc2_out)
+		defer close(err_out)
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				err_out <- fmt.Errorf("problem with APRS-IS socket: %w", err)
+				return
+			}
+			data := string(buffer[:n])
+			packets := strings.SplitSeq(data, "\r\n")
+			for packet := range packets {
+				if packet != "" && !strings.HasPrefix(packet, "#") {
+					tnc2_out <- packet
+
+				}
+			}
+
+		}
+	}()
+	return tnc2_out, err_out, nil
+
+}
+
 func ConsolePrinter(str <-chan string, err <-chan error) {
 	for {
 		select {
@@ -77,7 +120,8 @@ func main() {
 	flag.Parse()
 	fmt.Println(*serverPtr)
 
-	tnc2_out, err_out, err := ListenOnlyKISS(*serverPtr)
+	// tnc2_out, err_out, err := ListenOnlyKISS(*serverPtr)
+	tnc2_out, err_out, err := ListenOnlyAPRSIS()
 	if err != nil {
 		log.Fatalf("Failed to connect to server: %s", err)
 	}
